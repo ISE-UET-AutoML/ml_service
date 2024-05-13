29d47932f0a626ea597906ee2a3f0d67d660cc63
minhfli <vuvanminh.bacninh@gmail.com>
2024-05-13 12:59:44 +0700

    training service -> model service

--- app/training_service/local/object_detection/model.py
+++ app/training_service/local/object_detection/model.py
@@ -3,15 +3,10 @@ from nptyping import Byte
 from pydantic import BaseModel
 from pydantic.fields import Field
 
-from app.training_service.local.image_classifier.available_checkpoint import Timm_Checkpoint
+from app.training_service.training_request import TrainingRequest
 
-
-class TrainingRequest(BaseModel):
+class TrainingRequest(TrainingRequest):
     # Mỗi user là 1 bucket riêng trong GCS
-    userEmail: str = Field(default="lexuanan18102004", title="userEmail but loai bo @ to get the bucket => Have to be unique")
-    projectName: str = Field(default="flower-classifier", title="Project name")
-    training_time: int = Field(default=60, title="Training Time")
-    runName: str = Field(default="Namdeptraiqua", title="Run name")
     training_argument: dict = Field(default =
     {
         "data_args": {},

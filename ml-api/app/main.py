4f2dec5a200db47bf1495475de81409283c7a5f7
minhfli <vuvanminh.bacninh@gmail.com>
2024-05-19 18:01:57 +0700

    queued service branch

--- ml-api/app/main.py
+++ ml-api/app/main.py
@@ -3,34 +3,32 @@ from fastapi import FastAPI, File, UploadFile
 from contextlib import asynccontextmanager
 import torch
 
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 torch.cuda.empty_cache()
 torch.set_float32_matmul_precision("medium")
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from training_service import routes as training_service_routes
-#from training_service.image_classifier import routes as image_classifier_routes
-#from training_service.tabular.classification import routes as tabular_classifier_routes
+from model_service.train import routes as training_service_routes
 
+# ++++++++++++++++++++++++++++++++++++++++++++ DEFINE APP +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 app = FastAPI()
+app = FastAPI(title="ml_service", openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url="/api/redoc")
 
 
 @asynccontextmanager  # lifespan cua context, sau nay su dung de release resource sau khi serve xong model
 def init():
     pass
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 app.include_router(training_service_routes.router)
-#app.include_router(image_classifier_routes.router)
-#app.include_router(tabular_classifier_routes.router)
 
-
-# app.include_router(tabular_classifier_routes.router)
 
 # @serve.deployment(num_replicas=1, ray_actor_options={"num_cpus": 8, "num_gpus": 1})
 # @serve.ingress(app)

85f1ea9776fd1de724fc926e0abcf97e84880e81
minhfli <vuvanminh.bacninh@gmail.com>
2024-07-16 11:17:05 +0700

    feat: test stoping worker

--- ml-api/testing/api_call.py
+++ ml-api/testing/api_call.py
@@ -1,6 +1,5 @@
+import celery
+from config import celery_client
 
-import requests
-
-
-
-requests.post('http://localhost:8000/test_train',None )
+task_id = "e5085630-2a9c-47cf-8b3f-10bac30b4d12"
+celery_client.control.revoke(task_id, terminate=True)

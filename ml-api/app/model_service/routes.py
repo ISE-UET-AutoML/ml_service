d0a5d8c5331eb500e58a0e1c00bc0b5a98b1d5d7
minhfli <vuvanminh.bacninh@gmail.com>
2024-07-23 22:48:36 +0700

    feat: zeroshot image prediction

--- ml-api/app/model_service/routes.py
+++ ml-api/app/model_service/routes.py
@@ -2,11 +2,13 @@ from unittest import result
 import celery
 from fastapi import APIRouter
 from .train.routes_v1 import router as train_router
+from .autogluon_zeroshot.routes import router as zeroshot_router
 from settings.config import celery_client, redis_client
 from celery.result import AsyncResult
 
 router = APIRouter()
 router.include_router(train_router, prefix="/train")
+router.include_router(zeroshot_router, prefix="/zeroshot")
 
 
 @router.get("/cel_test")

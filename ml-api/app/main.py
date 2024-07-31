5f7de03a1ecd3e37f31ee27cf320a0e55dee7f5d
minhfli <vuvanminh.bacninh@gmail.com>
2024-07-31 08:52:21 +0700

    fix: cors

--- ml-api/app/main.py
+++ ml-api/app/main.py
@@ -11,8 +11,21 @@ from fastapi import FastAPI, Form
 from settings.config import celery_client
 
 from model_service.routes import router as model_service_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(debug=True)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")

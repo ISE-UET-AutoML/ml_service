48eca85d81b970f61e67c4610cd5ec41b6bb8cb1
minhfli <vuvanminh.bacninh@gmail.com>
2024-08-20 22:59:13 +0700

    quick fix before merge

--- ml-api/app/model_service/train/explainers/TextExplainer.py
+++ ml-api/app/model_service/train/explainers/TextExplainer.py
@@ -12,29 +12,34 @@ class TextExplainer(BaseExplainer):
     def __init__(self, method="shap", model=None, class_names=None):
         super().__init__(model, class_names)
         self.method = method
-        self.tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "distilbert/distilbert-base-uncased"
+        )
 
         # TODO: fix hardcode class names
         if self.class_names == [0, 1]:
             self.class_names = ["negative", "positive"]
         if method == "shap":
-            self.explainer = shap.Explainer(self.predict_proba, self.tokenizer, output_names=self.class_names)
-
+            self.explainer = shap.Explainer(
+                self.predict_proba, self.tokenizer, output_names=self.class_names
+            )
 
     def preprocess(self, instance):
         if self.method == "shap":
-            return pd.DataFrame([instance], columns=['sentence'])
+            return pd.DataFrame([instance], columns=["text"])
 
     def predict_proba(self, instances):
         sentences = []
         for instance in instances:
             sentences.append(instance)
-        return self.model.predict_proba({'sentence': sentences}, realtime=True)
+        return self.model.predict_proba({"text": sentences}, realtime=True)
 
     def explain(self, instance):
         data = self.preprocess(instance)
         if self.method == "shap":
-            shap_values = self.explainer(data['sentence'][0:1], max_evals=100, batch_size=20)
+            shap_values = self.explainer(
+                data["text"][0:1], max_evals=100, batch_size=20
+            )
             return shap.plots.text(shap_values, display=False)
         else:
             return "Method not supported"

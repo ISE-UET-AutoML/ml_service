e2228c08d3da4f97cc1284a418dfdaf8514418dc
Tuan_koi <tranduytuana123@gmail.com>
2024-11-20 23:12:27 +0700

    :mute:chore: change env

--- ml-celery/app/utils/aws.py
+++ ml-celery/app/utils/aws.py
@@ -58,7 +58,12 @@ def create_presigned_post(object_name, bucket_name=BUCKET_NAME,
     """
 
     # Generate a presigned S3 POST URL
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=BUCKET_REGION
+    )
     try:
         response = s3_client.generate_presigned_post(bucket_name,
                                                      object_name,
